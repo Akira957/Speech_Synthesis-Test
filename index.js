ed5043157d238911f6ef27a098e107174d5f3161
Robert Ramirez <metaknight757@gmail.com>
2022-02-28 11:29:47 -0600

    Javascript is done and the speech synthesis is working

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ msg.text = document.querySelector('[name="text"]').value;
 
 function populateVoices() {
     voices = this.getVoices();
-    const voiceOptions = voices.map((voice) => {
+    const voiceOptions = voices.map((voice) =>
         `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`
-    })
+    )
         .join("");
     voicesDropdown.innerHTML = voiceOptions;
 }
@@ -20,3 +20,23 @@ function setVoice() {
     msg.voice = voices.find((voice) => voice.name === this.value);
     toggle();
 }
+
+function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+        speechSynthesis.speak(msg);
+    }
+}
+
+function setOption() {
+    console.log(this.name, this.value);
+    msg[this.name] = this.value;
+    toggle();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+
+options.forEach((option) => option.addEventListener("change", setOption))
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle.bind(null, false));
